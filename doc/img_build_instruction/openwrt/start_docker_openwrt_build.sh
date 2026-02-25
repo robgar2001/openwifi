91c99a8060b3664c194553136c2524a3296b071a
Robbe Gaeremynck <robbe.gaeremynck@outlook.com>
2026-02-25 12:30:40 +0100

    Add start_docker_openwrt_build.sh

---
+++ doc/img_build_instruction/openwrt/start_docker_openwrt_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run --interactive --rm --tty --ulimit 'nofile=1024:262144' \
+--volume "$(pwd)/openwrt:/workdir" \
+--workdir '/workdir' openwrt:debian_12 /bin/bash
+<hostname>:/workdir$
